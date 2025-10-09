68fabad4408e531fadbb2132462680ff3f3e3181
Muhammad Arslan <mughalarslan996@gmail.com>
2025-10-09 18:41:35 +0500

    add authentication package and the laravel roles and permisson package setup

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            'super_admin',
+            'customer',
+            'employee',
+        ];
+
+        foreach ($roles as $roleName) {
+            Role::firstOrCreate(['name' => $roleName]);
+        }
+    }
+}
