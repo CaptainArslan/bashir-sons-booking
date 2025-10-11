902de91e8d62926f61df49570b2d6d4ffe6d87ed
Muhammad Arslan <mughalarslan996@gmail.com>
2025-10-11 18:17:48 +0500

    add website general settings table where we have all the settings regarding to the websiter

---
+++ app/Models/GeneralSetting.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class GeneralSetting extends Model
+{
+    /** @use HasFactory<\Database\Factories\GeneralSettingFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'company_name',
+        'email',
+        'phone',
+        'alternate_phone',
+        'address',
+        'city',
+        'state',
+        'country',
+        'postal_code',
+        'website_url',
+        'logo',
+        'favicon',
+        'tagline',
+        'facebook_url',
+        'instagram_url',
+        'twitter_url',
+        'linkedin_url',
+        'youtube_url',
+        'support_email',
+        'support_phone',
+        'business_hours',
+    ];
+
+    protected $casts = [
+        // 
+    ];
+}
