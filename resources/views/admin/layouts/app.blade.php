d9c48dcbe3f95f22fd972004ffb2f4eedd447700
Muhammad Arslan <mughalarslan996@gmail.com>
2025-10-13 10:15:18 +0500

    update the admin home page

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="{{ asset('admin/assets/images/favicon-32x32.png') }}" type="image/png">
+    <!--plugins-->
+    <link href="{{ asset('admin/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet">
+    <!-- loader-->
+    <link href="{{ asset('admin/assets/css/pace.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('admin/assets/js/pace.min.js') }}"></script>
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('admin/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/css/bootstrap-extended.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link href="{{ asset('admin/assets/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/css/icons.css') }}" rel="stylesheet">
+    <!-- Theme Style CSS -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/dark-theme.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/semi-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/header-colors.css') }}">
+    <title>@yield('title' ?? config('app.name'))</title>
+</head>
+
+<body>
+    <!--wrapper-->
+    <div class="wrapper">
+        @include('admin.layouts.sidebar')
+
+        @include('admin.layouts.header')
+        <!--start page wrapper -->
+        <div class="page-wrapper">
+            <div class="page-content">
+                @yield('content')
+            </div>
+        </div>
+        <!--end page wrapper -->
+        <!--start overlay-->
+        <div class="overlay toggle-icon"></div>
+        <!--end overlay-->
+        <!--Start Back To Top Button-->
+        <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
+        <!--End Back To Top Button-->
+
+        @include('admin.layouts.footer')
+
+    </div>
+
+    {{-- @include('admin.layouts.switcher') --}}
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('admin/assets/js/bootstrap.bundle.min.js') }}"></script>
+    <!--plugins-->
+    <script src="{{ asset('admin/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <script src="{{ asset('admin/assets/plugins/chartjs/js/chart.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/index.js') }}"></script>
+    <!--app JS-->
+    <script src="{{ asset('admin/assets/js/app.js') }}"></script>
+    {{-- <script>
+        new PerfectScrollbar(".app-container");
+    </script> --}}
+</body>
+
+{{-- <script>
+    'undefined' === typeof _trfq || (window._trfq = []);
+    'undefined' === typeof _trfd && (window._trfd = []), _trfd.push({
+        'tccl.baseHost': 'secureserver.net'
+    }, {
+        'ap': 'cpsh-oh'
+    }, {
+        'server': 'p3plzcpnl509132'
+    }, {
+        'dcenter': 'p3'
+    }, {
+        'cp_id': '10399385'
+    }, {
+        'cp_cl': '8'
+    })
+    // Monitoring performance to make your website faster. If you want to opt-out, please contact web hosting support.
+</script> --}}
+{{-- <script src='../../../signals/js/clients/scc-c2/scc-c2.min.js'></script> --}}
+
+</html>
